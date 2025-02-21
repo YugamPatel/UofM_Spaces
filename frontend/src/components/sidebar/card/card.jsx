aed9a4da1355880a7a085cb33f737f2fbbbac8bf
Vansh Rastogi <123031177+VanshR04@users.noreply.github.com>
2025-02-21 14:56:37 -0600

    updated data with image and made components

---
+++ frontend/src/components/sidebar/card/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function sidebar({data}) {
+    return (
+        <>
+            <div className="outer-card">
+                
+            </div>
+        </>
+    )
+}
---
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { spaces } from "../../Data/Test";
+
+function sidebar() {
+    return (
+        <>
+            <div className="outer-body">
+                
+            </div>
+        </>
+    )
+}
