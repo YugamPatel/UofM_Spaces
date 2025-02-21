17ff2ea0aa4c0b9c29c27bbd6a071c332adaee7e
Vansh Rastogi <123031177+VanshR04@users.noreply.github.com>
2025-02-21 14:44:27 -0600

    making data available outside file

--- frontend/src/Data/Test.js
+++ frontend/src/Data/Test.js
@@ -1,8 +1,17 @@
-spaces = [{
+export const spaces = [{
     "name": "EITC",
     "description": "Study spaces in the EITC complex at the U of M",
     "timings" : "07:00 - 22:00",
     "coordinates": [
         49.80841356933436, -97.13388687533251
     ]
-}]
+},
+{
+    "name": "Elizabeth Dafoe Library",
+    "description": "Study spaces in the Elizabeth Dafoa library at the U of M",
+    "timings" : "07:00 - 23:00",
+    "coordinates": [
+        49.8102231737049, -97.13173648596704
+    ]
+}
+]
