7946ddcd88797b7b22f35f108abc9c70dce119ba
Vansh Rastogi <123031177+VanshR04@users.noreply.github.com>
2025-02-21 17:09:25 -0600

    added collapsable components

---
+++ frontend/src/Data/Collapsable.js
@@ -0,0 +1,21 @@
+export const individual = [
+    {
+        "name" : "Quiet Pods",
+        "description" : "Quiet Pods are designed to provide a peaceful environment for studying",
+        "image" : "https://images.unsplash.com/20/cambridge.JPG?q=80&w=2294&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "timings" : [
+            {
+                "time":"10:00-11:00",
+                "status":"Available"
+            },
+            {
+                "time":"11:00-12:00",
+                "status":"Available"
+            },
+            {
+                "time":"12:00-13:00",
+                "status":"Available"
+            }
+        ]
+    }
+]
--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { spaces } from "../../Data/Test";
 import Card from "./card/card";
 import "../sidebar/sidebar.css";
+import Collapsable from "./collapsable/collapsable";
 
 function Sidebar() {
   return (
@@ -10,6 +11,7 @@ function Sidebar() {
         {spaces.map((space) => {
           return <Card {...space} />;
         })}
+        <Collapsable />
       </div>
     </>
   );
